d962825c73c5246fbec118c0706758bf04a946c9
iOSDeveloperGuy <101750471+iOSDeveloperGuy@users.noreply.github.com>
2026-07-14 12:33:11 -0400

    Report embedded Go build version

--- cmd/jui/main.go
+++ cmd/jui/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"runtime/debug"
 
 	"github.com/iOSDeveloperGuy/jui/internal/justfile"
 	"github.com/iOSDeveloperGuy/jui/internal/ui"
@@ -16,26 +17,7 @@ func main() {
 	showVersion := flag.Bool("version", false, "print version")
 	flag.Parse()
 	if *showVersion {
-		fmt.Println("jui", version)
+		fmt.Println("jui", buildVersion())
 		return
 	}
 	if _, err := exec.LookPath("just"); err != nil {
-		fatal("just is not installed or not on PATH")
-	}
-	if _, err := exec.LookPath("stty"); err != nil {
-		fatal("stty is required")
-	}
-	path, err := justfile.Discover(".")
-	if err != nil {
-		fatal(err.Error())
-	}
-	recipes, err := justfile.Parse(path)
-	if err != nil {
-		fatal(err.Error())
-	}
-	if err := ui.New(path, recipes).Run(); err != nil {
-		fatal(err.Error())
-	}
-}
-
-func fatal(message string) { fmt.Fprintln(os.Stderr, "jui:", message); os.Exit(1) }
